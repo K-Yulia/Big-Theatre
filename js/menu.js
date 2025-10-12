28bd0e20e7a56efe3f38ce81f71a650391d90eac
K-Yulia <109465660+K-Yulia@users.noreply.github.com>
2025-10-12 15:23:30 +0300

    fix scroll

--- js/menu.js
+++ js/menu.js
@@ -1,46 +1,42 @@
-(() => {
+ (() => {
   const refs = {
     openMenuBtn: document.querySelector('[data-menu-open]'),
     closeMenuBtn: document.querySelector('[data-menu-close]'),
     menu: document.querySelector('[data-menu]'),
     header: document.querySelector(".page-header"), 
+    navList: document.querySelector('.js-nav'),
   };
 
   refs.openMenuBtn.addEventListener('click', toggleModal);
   refs.closeMenuBtn.addEventListener('click', toggleModal);
-
+  refs.navList.addEventListener('click', onListClick);
+  
   function toggleModal() {
-   nav = refs.menu.classList.toggle('is-open');
+    nav = refs.menu.classList.toggle('is-open');
     if (nav) {
       document.body.style.overflow = 'hidden'; 
       refs.header.classList.add("menu-open");
     } else { document.body.style.overflow = 'scroll'; 
       refs.header.classList.remove('menu-open');
-    }   
-}
+    } 
+  }
 }
 )();
-
-const refs = {
-  burgerMenu: document.querySelector('.js-menu'),
-  navList: document.querySelector('.js-nav'),
-};
-
-refs.navList.addEventListener('click', onListClick);
 
 function onListClick(ev) {
   refs.burgerMenu.classList.remove('is-open');
   refs.header.classList.remove("menu-open");
-  }
+}
 
-
-
-  window.addEventListener("scroll", function () {
+window.addEventListener("scroll", function () {
   const header = document.querySelector(".page-header");
   if (window.scrollY > 70) {
     header.classList.add("scrolled");
   } else {
     header.classList.remove("scrolled");
   }
-});
+}
+);
   
+
+
